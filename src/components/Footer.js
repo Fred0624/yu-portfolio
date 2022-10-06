053949ba20625a3cba6e81919ac9bda34089dccb
Fred0624 <fred10150912@hotmail.com>
2022-10-06 14:00:35 -0700

    footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,18 @@ import Github from "@material-ui/icons/GitHub"
 import LinkedInIcon from "@material-ui/icons/LinkedIn"
 import EmailIcon from "@material-ui/icons/Email"
 import '../styles/Footer.css'
+import Link from '@material-ui/core/Link'
 
 const Footer = () => {
     return (
         <div className='footer'>
             <div className='socialLinks'>
+                <Link href='https://www.linkedin.com/in/yu-xiong/'>
                 <LinkedInIcon />
+                </Link>
+                <Link href='https://github.com/Fred0624'>
                 <Github />
+                </Link>
                 <EmailIcon />
             </div>
             <p>©Copyright {new Date().getFullYear()}</p>
