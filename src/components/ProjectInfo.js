9b846cbe2709ed74535c8e41de18bc3174f0bda8
Fred0624 <fred10150912@hotmail.com>
2022-10-12 15:26:15 -0700

    live links

--- src/components/ProjectInfo.js
+++ src/components/ProjectInfo.js
@@ -8,17 +8,20 @@ export const projectInfo = [
         image: BreakingBad,
         skill: "HTML, CSS, JavaScript, React, React Bootstrap",
         link: "https://github.com/Fred0624/Breaking-Bad",
+        live: "https://breaking-bad-123.netlify.app/",
     },
     {
         name: "Jupiter Deals",
         image: JupiterDeals,
         skill: "Axios, React Material UI, React Router Dom, React Bootstrap, Heroku, MongoDB, Mongoose, Express, React, Puppeteer, Cheerio, Browserless",
         link: "https://github.com/Jupiter115/crispy-guide",
+        live: "https://jupiterdeals.netlify.app/"
     },
     {
         name: "RetroDash",
         image: RetroDash,
         skill: "HTML, CSS, JavaScript, THREE.JS, Blender, Vite",
         link: "https://github.com/runner-runner224/running-square",
+        live: "https://retrodash.netlify.app/"
     }
 ]
